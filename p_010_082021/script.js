e044dae5235f974afc6d5df576b0289207cc145e
Whoin Lee <whoinlee@gmail.com>
2021-08-19 12:07:49 -0400

    p10 complete

--- p_010_082021/script.js
+++ p_010_082021/script.js
@@ -1,17 +1,4 @@
-// const loadText = document.querySelector('.loading-text');
-// const bg = document.querySelector('.bg');
-// const scale = (num, in_min, in_max, out_min, out_max) => {
-//     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-// }
+const toggleBtn = document.getElementById('toggle')
+const nav = document.getElementById('nav')
 
-// let load = 0;
-// let int = setInterval(blurring, 30);
-// function blurring() {
-//     load++;
-//     loadText.innerText = `${load}%`;
-//     loadText.style.opacity = (100 - load) / 100;
-//     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
-//     if (load > 99) clearInterval(int);
-//     // console.log(load);
-// }
-
+toggleBtn.addEventListener('click', () => nav.classList.toggle('active'))
