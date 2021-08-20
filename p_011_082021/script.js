bde25380f484506b0a22543e2dc066885c38f6d5
Whoin Lee <whoinlee@gmail.com>
2021-08-20 18:39:13 -0400

    p11 complete

--- p_011_082021/script.js
+++ p_011_082021/script.js
@@ -2,8 +2,12 @@ const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 const container = document.querySelector('.container')
 
-left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseenter', () => {
+    container.classList.add('hover-left');
+})
 left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
 
-right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener('mouseenter', () => {
+    container.classList.add('hover-right');
+})
 right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
