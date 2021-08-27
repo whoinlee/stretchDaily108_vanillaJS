956537f6ff6d72cb5af6d6202bd8cfde713e0a7e
Whoin Lee <whoinlee@gmail.com>
2021-08-27 13:41:06 -0400

    theme color change

--- p_016_082721 /script.js
+++ p_016_082721 /script.js
@@ -1,14 +1,22 @@
+const html = document.querySelector('html')
 const toggleBtn = document.getElementById('themeToggleBtn');
-// console.log("toggleButton? ", toggleBtn);
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
 
 toggleBtn.addEventListener('click', (e) => {
     // console.log("onClick");
-    const html = document.querySelector('html')
+    
     if (html.classList.contains('dark')) {
-        html.classList.remove('dark')
-        e.target.innerHTML = 'Dark Mode'
+        html.classList.remove('dark');
+        e.target.innerHTML = 'Dark Mode';
+        e.target.classList.remove('light');   
+        hourEl.classList.remove('light');
+        minuteEl.classList.remove('light');
     } else {
-        html.classList.add('dark')
-        e.target.innerHTML = 'Light Mode'
+        html.classList.add('dark');
+        e.target.innerHTML = 'Light Mode';
+        e.target.classList.add('light');
+        hourEl.classList.add('light'); 
+        minuteEl.classList.add('light'); 
     }
 });
