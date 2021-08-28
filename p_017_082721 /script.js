2e0c55902bafbb69d518fc7748df2bd9fdb4543e
Whoin Lee <whoinlee@gmail.com>
2021-08-27 20:26:41 -0400

    p17 complete

---
+++ p_017_082721 /script.js
@@ -0,0 +1,24 @@
+
+setInterval(setTime, 1000);
+
+const hourHand = document.getElementById('hour-hand');
+const minuteHand = document.getElementById('minute-hand');
+const secondHand = document.getElementById('second-hand');
+
+function setTime() {
+    const time = new Date();
+    // console.log("time?", time)
+    const secondDeg = time.getSeconds()/60;
+    const minuteDeg = (secondDeg + time.getMinutes())/60;
+    const hourDeg = (minuteDeg + time.getHours())/12;
+
+    handsDegs(hourHand, hourDeg);
+    handsDegs(minuteHand, minuteDeg);
+    handsDegs(secondHand, secondDeg);
+}
+
+function handsDegs(elt, rotationRatio) {
+    elt.style.setProperty('--rotation', rotationRatio * 360);
+}
+
+setTime();
