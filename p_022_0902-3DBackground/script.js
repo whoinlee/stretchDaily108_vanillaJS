ce4f7f8146383eb886d3697af55fcfc1b540e91d
Whoin Lee <whoinlee@gmail.com>
2021-09-02 10:34:36 -0400

    p22 complete

--- p_022_0902-3DBackground/script.js
+++ p_022_0902-3DBackground/script.js
@@ -5,12 +5,13 @@ const btn = document.getElementById('btn')
 btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
 
 function createBoxes() {
+    //-- set 16 backgrounds
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
-      const box = document.createElement('div')
-      box.classList.add('box')
-      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
-      boxesContainer.appendChild(box)
+      const box = document.createElement('div');
+      box.classList.add('box');
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`; //******/
+      boxesContainer.appendChild(box);
     }
   }
 }
