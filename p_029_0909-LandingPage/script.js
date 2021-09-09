a1850c11ab48eaf214279cdeac88bd4d15f9020e
Whoin Lee <whoinlee@gmail.com>
2021-09-09 08:24:50 -0400

    p29 set up

--- p_029_0909-LandingPage/script.js
+++ p_029_0909-LandingPage/script.js
@@ -1,6 +1,21 @@
-const open = document.getElementById('open');
-const close = document.getElementById('close');
-const container = document.querySelector('.container');
+const open = document.getElementById('open');       //-- sign up
+const close = document.getElementById('close');     //-- close button on the modal
+const toggle = document.getElementById('toggle');   //-- nav sideBar open/close
+//
+const navbar = document.getElementById('navbar');
+const modal = document.getElementById('modal');
 
-open.addEventListener('click', () => container.classList.add('show-nav'));
-close.addEventListener('click', () => container.classList.remove('show-nav'));
+//-- Show/Hide modal
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', e =>
+  e.target == modal ? modal.classList.remove('show-modal') : false
+); //-- hide modal on outside click
+
+//-- Show/Hide nav
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav');
+    document.body.addEventListener('click', closeNavbar);
+});
+
+
