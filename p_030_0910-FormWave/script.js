3c631add5edc80df10c4599490e866e316ed0aea
Whoin Lee <whoinlee@gmail.com>
2021-09-10 11:10:25 -0400

    p30 complete

--- p_030_0910-FormWave/script.js
+++ p_030_0910-FormWave/script.js
@@ -2,7 +2,7 @@ const labels = document.querySelectorAll('.form-control label')
 
 labels.forEach(label => {
     label.innerHTML = label.innerText
-        .split('')
-        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
-        .join('')
+        .split('')  //-- array
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)  //******/
+        .join('')   //*****/
 })
