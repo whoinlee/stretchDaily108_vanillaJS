815285d36c06b7cdcad19ac8ef5660b72e334a27
Whoin Lee <whoinlee@gmail.com>
2021-09-16 08:21:24 -0400

    p35 complete

--- p_035_0916-DrinkWater/script.js
+++ p_035_0916-DrinkWater/script.js
@@ -1,20 +1,52 @@
-const counters = document.querySelectorAll('.counter');
+const smallCups = document.querySelectorAll('.cup-small');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
 
-counters.forEach(counter => {
-    counter.innerText = '0';
+updateBigCup()
 
-    const updateCounter = () => {
-        const target = +counter.getAttribute('data-target');
-        const c = +counter.innerText; //number
-        const increment = target / 200;
+smallCups.forEach((cup, idx) => {
+    cup.addEventListener('click', () => highlightCups(idx));
+})
 
-        if(c < target) {
-            counter.innerText = `${Math.ceil(c + increment)}`;
-            setTimeout(updateCounter, 1);
-        } else {
-            counter.innerText = target;
-        }
+function highlightCups(idx) {
+    // alert("idx: " + idx)
+    if (idx===7 && smallCups[idx].classList.contains("full")) {
+        idx--;  
+    }
+    else if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx--
     }
 
-    updateCounter();
-})
+    smallCups.forEach((cup, idx2) => {
+        if(idx2 <= idx) {
+            cup.classList.add('full')
+        } else {
+            cup.classList.remove('full')
+        }
+    })
+
+    updateBigCup()
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length;
+    const totalCups = smallCups.length;
+
+    if(fullCups === 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`;    //-- 330px: .cup height
+        percentage.innerText = `${fullCups / totalCups * 100}%`
+    }
+
+    if(fullCups === totalCups) {
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+    } else {
+        remained.style.visibility = 'visible'
+        liters.innerText = `${2 - (250 * fullCups / 1000)}L`
+    }
+}
