3961c4c6f63bd97551472b45476f258a59892739
Whoin Lee <whoinlee@gmail.com>
2021-09-18 10:15:50 -0400

    p37 complete

--- p_037_0918-BackgroundSlider/script.js
+++ p_037_0918-BackgroundSlider/script.js
@@ -1,40 +1,33 @@
-const body = document.body
-const slides = document.querySelectorAll('.slide')
-const leftBtn = document.getElementById('left')
-const rightBtn = document.getElementById('right')
+const body = document.body;
+const slides = document.querySelectorAll('.slide');
+const leftBtn = document.getElementById('leftBtn');
+const rightBtn = document.getElementById('rightBtn');
 
-let activeSlide = 0
+let activeSlideIndex = 0;
+setBgToBody();
 
 rightBtn.addEventListener('click', () => {
-  activeSlide++
-
-  if (activeSlide > slides.length - 1) {
-    activeSlide = 0
-  }
-
-  setBgToBody()
-  setActiveSlide()
-})
-
+    activeSlideIndex++;
+    if (activeSlideIndex > slides.length - 1) {
+        activeSlideIndex = 0
+    }
+    setBgToBody();
+    setActiveSlide();
+});
 leftBtn.addEventListener('click', () => {
-  activeSlide--
-
-  if (activeSlide < 0) {
-    activeSlide = slides.length - 1
-  }
-
-  setBgToBody()
-  setActiveSlide()
-})
-
-setBgToBody()
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+        activeSlideIndex = slides.length - 1
+    }
+    setBgToBody();
+    setActiveSlide();
+});
 
 function setBgToBody() {
-  body.style.backgroundImage = slides[activeSlide].style.backgroundImage
-}
-
+    //-- copy src
+    body.style.backgroundImage = slides[activeSlideIndex].style.backgroundImage;
+};
 function setActiveSlide() {
-  slides.forEach((slide) => slide.classList.remove('active'))
-
-  slides[activeSlide].classList.add('active')
-}
+  slides.forEach((slide) => slide.classList.remove('active'));
+  slides[activeSlideIndex].classList.add('active');
+};
