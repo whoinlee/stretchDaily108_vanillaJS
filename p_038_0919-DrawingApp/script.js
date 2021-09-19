7a8e7736a6c296e386713bfd043b55bd2946c60d
Whoin Lee <whoinlee@gmail.com>
2021-09-19 07:06:14 -0400

    p38 complete

--- p_038_0919-DrawingApp/script.js
+++ p_038_0919-DrawingApp/script.js
@@ -7,7 +7,10 @@ const clearBtn = document.getElementById('clearBtn');
 
 const ctx = canvas.getContext('2d');    //****/
 
-let size = 10;
+const sizeOffset = 5;
+const maxSize = 50;
+const minSize = 5;
+let size = 10;  //-- radius == half of line thickness
 let isPressed = false;
 colorInput.value = 'black';
 let color = colorInput.value;
@@ -15,69 +18,65 @@ let x;
 let y;
 
 canvas.addEventListener('mousedown', (e) => {
-    isPressed = true
-
-    x = e.offsetX
-    y = e.offsetY
+    isPressed = true;
+    x = e.offsetX;
+    y = e.offsetY;
 })
 
 document.addEventListener('mouseup', (e) => {
-    isPressed = false
-
-    x = undefined
-    y = undefined
+    isPressed = false;
+    x = undefined;
+    y = undefined;
 })
 
 canvas.addEventListener('mousemove', (e) => {
     if(isPressed) {
-        const x2 = e.offsetX
-        const y2 = e.offsetY
+        const x2 = e.offsetX;
+        const y2 = e.offsetY;
 
-        drawCircle(x2, y2)
-        drawLine(x, y, x2, y2)
+        drawCircle(x2, y2);
+        drawLine(x, y, x2, y2);
 
-        x = x2
-        y = y2
+        x = x2;
+        y = y2;
     }
 })
 
 function drawCircle(x, y) {
     ctx.beginPath();
-    ctx.arc(x, y, size, 0, Math.PI * 2)
-    ctx.fillStyle = color
-    ctx.fill()
+    ctx.arc(x, y, size, 0, Math.PI * 2);
+    ctx.fillStyle = color;
+    ctx.fill();
 }
 
 function drawLine(x1, y1, x2, y2) {
-    ctx.beginPath()
-    ctx.moveTo(x1, y1)
-    ctx.lineTo(x2, y2)
-    ctx.strokeStyle = color
-    ctx.lineWidth = size * 2
-    ctx.stroke()
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size * 2;
+    ctx.stroke();
 }
 
 function updateSizeOnScreen() {
-    sizeEL.innerText = size
+    sizeEL.innerText = size;
 }
 
 increaseBtn.addEventListener('click', () => {
-    size += 5
-
-    if(size > 50) {
-        size = 50
+    size += sizeOffset;
+    if(size > maxSize) {
+        size = maxSize;
     }
 
-    updateSizeOnScreen()
+    updateSizeOnScreen();
 });
 decreaseBtn.addEventListener('click', () => {
-    size -= 5
-
-    if(size < 5) {
-        size = 5
+    size -= sizeOffset;
+    if(size < minSize) {
+        size = minSize;
     }
 
-    updateSizeOnScreen()
+    updateSizeOnScreen();
 });
 colorInput.addEventListener('change', (e) => color = e.target.value);
 clearBtn.addEventListener('click', () => ctx.clearRect(0,0, canvas.width, canvas.height));
