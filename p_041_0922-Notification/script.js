ea7acd1675ee8d8f9758a167783c7a128562a6cd
Whoin Lee <whoinlee@gmail.com>
2021-09-22 07:59:54 -0400

    p41 complete

--- p_041_0922-Notification/script.js
+++ p_041_0922-Notification/script.js
@@ -10,12 +10,10 @@ const types = ['info', 'success', 'error'];
 
 button.addEventListener('click', () => createNotification());
 function createNotification(message = null, type = null) {
-    const notif = document.createElement('div')
-    notif.classList.add('toast')
-    notif.classList.add(type ? type : getRandomType())
-
-    notif.innerText = message ? message : getRandomMessage()
-
+    const notif = document.createElement('div');
+    notif.classList.add('toast');
+    notif.classList.add(type ? type : getRandomType());
+    notif.innerText = message ? message : getRandomMessage();
     toasts.appendChild(notif)
 
     setTimeout(() => {
