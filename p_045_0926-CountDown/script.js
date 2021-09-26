07366430adf234e4378d6b53573126c99330aaf3
Whoin Lee <whoinlee@gmail.com>
2021-09-26 08:25:22 -0400

    p45 complete

--- p_045_0926-CountDown/script.js
+++ p_045_0926-CountDown/script.js
@@ -3,38 +3,37 @@ const finalHolder = document.querySelector('.final');
 const nums = document.querySelectorAll('.nums span');
 const replayBtn = document.querySelector('#replay');
 
-runAnimation();
-
 function resetDOM() {
-    counterHolder.classList.remove('hide')
-    finalHolder.classList.remove('show')
-
-  nums.forEach((num) => {
-    num.classList.value = ''
-  })
-
-  nums[0].classList.add('in')
-}
-
-function runAnimation() {
-  nums.forEach((num, idx) => {
-    const nextToLast = nums.length - 1
-
-    num.addEventListener('animationend', (e) => {
-      if (e.animationName === 'goIn' && idx !== nextToLast) {
-        num.classList.remove('in')
-        num.classList.add('out')
-      } else if (e.animationName === 'goOut' && num.nextElementSibling) {
-        num.nextElementSibling.classList.add('in')
-      } else {
-        counterHolder.classList.add('hide')
-        finalHolder.classList.add('show')
-      }
+    counterHolder.classList.remove('hide');
+    finalHolder.classList.remove('show');
+    nums.forEach((numSpan) => {
+        numSpan.classList.value = '';   //-- span
     })
-  })
+    nums[0].classList.add('in');
 }
+
+//***********/
+function runAnimation() {
+    const lastIndex = nums.length - 1;
+    nums.forEach((num, idx) => {
+        //***** animationed event??? 
+        num.addEventListener('animationend', (e) => {
+            if (e.animationName === 'goIn' && idx !== lastIndex) {
+                num.classList.remove('in');
+                num.classList.add('out');
+            } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+                num.nextElementSibling.classList.add('in'); //*****nextElementSibling */
+            } else {//-- the last one
+                counterHolder.classList.add('hide');
+                finalHolder.classList.add('show');
+            }
+        });
+    });
+};
 
 replayBtn.addEventListener('click', () => {
-  resetDOM()
-  runAnimation()
-})
+  resetDOM();
+  runAnimation();
+});
+
+runAnimation();
