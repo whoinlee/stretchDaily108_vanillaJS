0603f2685a8044422aaed4ccc4c9cae0fdc23abc
Whoin Lee <whoinlee@gmail.com>
2021-09-29 11:23:45 -0400

    cleaned up

--- p_048_0929-CustomSlider/script.js
+++ p_048_0929-CustomSlider/script.js
@@ -6,18 +6,18 @@ const num_range_width = +range_width.substring(0, range_width.length - 2);    //
 const num_label_width = +label_width.substring(0, label_width.length - 2);
 const max = +slider.max;
 const min = +slider.min;
-const label_left_min = min - num_label_width/2 + 12;    //12 = thumb_width/2
-const label_left_max = num_range_width - num_label_width/2 - 12;
+//0: slider left location
+const label_left_min = 0 - num_label_width/2 + 12;    //12 = thumb_width/2
+const label_left_max = 0 + num_range_width - num_label_width/2 - 12;
 // console.log("max, min", max)
 // console.log("max, min", min)
-console.log("label_left_min", label_left_min)
-console.log("label_left_max", label_left_max)
+// console.log("label_left_min", label_left_min)
+// console.log("label_left_max", label_left_max)
 // console.log("range_width", range_width);    //300px, also set in the css
 // console.log("label_width", label_width);    //80px, also set in the css
 
 slider.addEventListener('input', (e) => {
     const value = +e.target.value;  // 0 to 100, min to max
-    // const left = value * (num_range_width / (max-min)) - num_label_width / 2 + scale(value, min, max, 10, -10);
     //-- slider thumb doesn't move from 0(min) to 300(num_range_width)
     //-- from (0 + thumb_width/2) to (100 - thumb_width/2)
     const left = scale(value, min, max, label_left_min, label_left_max);
