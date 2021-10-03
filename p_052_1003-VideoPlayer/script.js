a90982ab789830450ccc2e0bd8181d4ca4b530ff
Whoin Lee <whoinlee@gmail.com>
2021-10-03 14:17:01 -0400

    overlay & title show/hide

--- p_052_1003-VideoPlayer/script.js
+++ p_052_1003-VideoPlayer/script.js
@@ -1,17 +1,30 @@
 const preloader = document.querySelector(".preloader");
+const container = document.querySelector(".container");
+const overlay = document.querySelector(".overlay");
+
 const video = document.querySelector(".video-holder");
+const title = document.querySelector(".title");
 const playPauseBtn = document.querySelector(".play-pause");
 
 playPauseBtn.addEventListener("click", function () {
   if (!playPauseBtn.classList.contains("slide")) {
     playPauseBtn.classList.add("slide");
+    overlay.classList.add("show");
+    title.classList.remove("hide");
     video.pause();
   } else {
     playPauseBtn.classList.remove("slide");
+    overlay.classList.remove("show");
+    title.classList.add("hide");
     video.play();
   }
 });
 
 window.addEventListener("load", function () {
-  preloader.classList.add("hide-preloader");
+  // preloader.classList.add("hide-preloader");
+  this.setTimeout(hidePreloader, 1000); //-- show preloader for a second
 });
+
+function hidePreloader() {
+  preloader.classList.add("hide-preloader");
+}
