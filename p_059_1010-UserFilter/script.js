8ae1cae0b3a2d05c51dca830ff5deee77c08a1a1
Whoin Lee <whoinlee@gmail.com>
2021-10-12 20:21:58 -0400

    p59 complete

--- p_059_1010-UserFilter/script.js
+++ p_059_1010-UserFilter/script.js
@@ -1,19 +1,20 @@
-
-const result = document.getElementById('result');
-const filter = document.getElementById('filter');
-const listItems = [];
-
 const randomUserURL = 'https://randomuser.me/api/';
+
+const filterInput = document.getElementById('filter');
+const resultUl = document.getElementById('result');
+
+const listItems = [];
 const numOfUsers = 20;
+
 const fetchUsers = async () => {
   const data = await (await fetch(`${randomUserURL}?results=${numOfUsers}`)).json();
-  console.log("script.js :: fetchUsers, data??\n", data);
+//   console.log("script.js :: fetchUsers, data??\n", data);
   return data.results;
 }
 
 const init = async () => {
     const users = await fetchUsers(); //-- an array of [numOfUsers] objects
-    result.textContent = ''; //-- remove all existing child elts
+    resultUl.textContent = ''; //-- remove all existing child elts
     users.forEach(user => {
         const li = document.createElement('li');
         listItems.push(li);
@@ -23,7 +24,7 @@ const init = async () => {
                 <h4>${user.name.first} ${user.name.last}</h4>
                 <p>${user.location.city}, ${user.location.country}</p>
             </div>`;
-        result.appendChild(li)
+        resultUl.appendChild(li)
     });
 };
 
@@ -36,6 +37,6 @@ function filterData(searchTerm) {
         }
     })
 }
-filter.addEventListener('input', (e) => filterData(e.target.value));
+filterInput.addEventListener('input', (e) => filterData(e.target.value));
 
 init();
