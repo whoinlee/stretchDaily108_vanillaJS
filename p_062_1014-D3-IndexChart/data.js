b41a26d4a3e17c3133ef02665f25bdf32cfe4369
Whoin Lee <whoinlee@gmail.com>
2021-10-15 11:40:45 -0400

    data.js complete

--- p_062_1014-D3-IndexChart/data.js
+++ p_062_1014-D3-IndexChart/data.js
@@ -1,35 +1,20 @@
-// let series = [];
-// const data = d3.merge(await Promise.all(
-//     [   d3.csv("./data/AAPL10yrs.csv"), 
-//         d3.csv("./data/AMZN10yrs.csv"), 
-//         d3.csv("./data/GOOG10yrs.csv"), 
-//         d3.csv("./data/IBM10yrs.csv"), 
-//         d3.csv("./data/MSFT10yrs.csv")  ]
-//         .map(async file => d3.csvParse(await file.text(), d => {
-//             const date = parseDate(d["Date"]);
-//             return {name: file.name.slice(0, -4), date, value: +d["Close"]};
-//     })))
-// );
-const row = d => {
-    d.value = +d['Close'];
-    d.date = d['Date'];
-    return d;
-};
-const fileName = ["AAPL", "AMZN", "GOOG", "IBM", "MSFT"];
-let resultArr = [];
+const fileName1 = ["AAPL", "AMZN", "GOOG", "IBM", "MSFT"];
+const fileName2 = ["APPLE", "AMAZN", "GOOGL", "IBM", "MSOFT"];
+const resultArr = [];
 await Promise.all(
-        [   d3.csv(`./data/${fileName[0]}10yrs.csv`, row), 
-            d3.csv(`./data/${fileName[1]}10yrs.csv`, row), 
-            d3.csv(`./data/${fileName[2]}10yrs.csv`, row), 
-            d3.csv(`./data/${fileName[3]}10yrs.csv`, row), 
-            d3.csv(`./data/${fileName[4]}10yrs.csv`, row)  ])  
+        [   d3.csv(`./data/${fileName1[0]}10yrs.csv`), 
+            d3.csv(`./data/${fileName1[1]}10yrs.csv`), 
+            d3.csv(`./data/${fileName1[2]}10yrs.csv`), 
+            d3.csv(`./data/${fileName1[3]}10yrs.csv`), 
+            d3.csv(`./data/${fileName1[4]}10yrs.csv`)  ])  
         .then (files => files.forEach((csvFile, i) => {
-                const dataArr = csvFile.map(item => {
-                    return {name:fileName[i], date: item.date, value: item.value}
-                });
-                resultArr.push(dataArr);
+            const dataArr = csvFile.map(item => {
+                return {name: fileName2[i], date: item['Date'], value: +item['Close']}
+            });
+            resultArr.push(dataArr);
         })
 );
+
 const data = d3.merge(resultArr);
 
 export default data;
--- p_062_1014-D3-IndexChart/script.js
+++ p_062_1014-D3-IndexChart/script.js
@@ -1,10 +1,58 @@
 import data from './data.js';
-console.log("script.js :: data??\n", data);
+// console.log("script.js :: data??\n", data);
+// const test = d3.groups(data, d => d.name);
+// console.log("script.js :: test\n", test);
+
+const dataByCom = d3.groups(data, d => d.name).map(([key, values]) => {
+    const v = values[0].value;
+    return {key, values: values.map(({date, value}) => ({date, value: value / v}))};
+});
+console.log("script.js :: dataByCom??\n", dataByCom);
 
 const margin = {top: 20, right: 40, bottom: 30, left: 40};
 const height = document.querySelector('.chart').offsetHeight; 
-// console.log("height?", height);
+const width = document.querySelector('.chart').offsetWidth; 
+// console.log("width?", width);    //1170
+// console.log("height?", height);  //600
 // console.log("script.js :: d3?? ", d3)
+
+const k = d3.max(d3.group(data, d => d.name), ([, group]) => d3.max(group, d => d.value) / d3.min(group, d => d.value));
+
+const x = d3.scaleUtc()
+    .domain(d3.extent(data, d => d.date))
+    .range([margin.left, width - margin.right])
+    .clamp(true);
+
+const y = d3.scaleLog()
+    .domain([1 / k, k])
+    .rangeRound([height - margin.bottom, margin.top]);
+
+const z = d3.scaleOrdinal(d3.schemeCategory10).domain(data.map(d => d.name));
+
+const xAxis = g => g
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
+    .call(g => g.select(".domain").remove());
+
+const yAxis = g => g
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y)
+    .ticks(null, x => +x.toFixed(6) + "×"))
+    .call(g => g.selectAll(".tick line").clone()    
+    .attr("stroke-opacity", d => d === 1 ? null : 0.2)
+    .attr("x2", width - margin.left - margin.right))
+    .call(g => g.select(".domain").remove());
+
+const parseDate = d3.utcParse("%Y-%m-%d");
+const formatDate = d3.utcFormat("%B, %Y");
+
+const line = d3.line()
+    .x(d => x(d.date))
+    .y(d => y(d.value));
+
+const bisect = d3.bisector(d => d.date).left;
+
+
 
 const svg = d3.select("svg")
     .on("mousemove", moved);
@@ -15,13 +63,34 @@ const rule = svg.append("g")
     .attr("y2", 0)
     .attr("stroke", "black");
   
-// const serie = svg.append("g")
-//     .style("font", "bold 10px sans-serif")
-//     .selectAll("g")
-//     .data(series)
-//     .join("g");
+const series = svg.append("g")
+    .style("font", "bold 10px sans-serif")
+    .selectAll("g")
+    .data(dataByCom)
+    .join("g");
 
-// serie.append("text")
+series.append("path")
+    .attr("fill", "none")
+    .attr("stroke-width", 1.5)
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-linecap", "round")
+    .attr("stroke", d => z(d.key))
+    .attr("d", d => line(d.values));
+
+// series.append("text")
+//     .datum(d => ({key: d.key, value: d.values[d.values.length - 1].value}))
+//     .attr("fill", "none")
+//     .attr("stroke", "white")
+//     .attr("stroke-width", 3)
+//     .attr("x", x.range()[1] + 3)
+//     .attr("y", d => y(d.value))
+//     .attr("dy", "0.35em")
+//     .text(d => d.key)
+//     .clone(true)
+//     .attr("fill", d => z(d.key))
+//     .attr("stroke", null);
+
+// series.append("text")
 //     .datum(d => ({key: d.key, value: d.values[d.values.length - 1].value}))
 //     .attr("fill", "none")
 //     .attr("stroke", "white")
@@ -52,12 +121,12 @@ function update(date) {
     console.log("script.js :: update");
     // date = d3.utcDay.round(date);
     // rule.attr("transform", `translate(${x(date) + 0.5},0)`);
-    // serie.attr("transform", ({values}) => {
+    // series.attr("transform", ({values}) => {
     //   const i = bisect(values, date, 0, values.length - 1);
     //   return `translate(0,${y(1) - y(values[i].value / values[0].value)})`;
     // });
     // svg.property("value", date).dispatch("input");
-  }
+}
 
 
 init();
