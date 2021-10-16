629c83aa33ace79d5658dd1d095f7b834d8fe3a2
Whoin Lee <whoinlee@gmail.com>
2021-10-15 21:49:25 -0400

    clean up

--- p_062_1014-D3-IndexChart/data.js
+++ p_062_1014-D3-IndexChart/data.js
@@ -18,3 +18,19 @@ await Promise.all(
 const data = d3.merge(resultArr);
 
 export default data;
+
+/*
+await Promise.all(
+        [   d3.csv(`./data/${fileName1[0]}10yrs.csv`), 
+            d3.csv(`./data/${fileName1[1]}10yrs.csv`), 
+            d3.csv(`./data/${fileName1[2]}10yrs.csv`), 
+            d3.csv(`./data/${fileName1[3]}10yrs.csv`), 
+            d3.csv(`./data/${fileName1[4]}10yrs.csv`)  ])  
+        .then (files => files.forEach((csvFile, i) => {
+            const dataArr = csvFile.map(item => {
+                return {name: fileName2[i], date: item['Date'], value: +item['Close']}
+            });
+            resultArr.push(dataArr);
+        })
+);
+*/
--- p_062_1014-D3-IndexChart/script.js
+++ p_062_1014-D3-IndexChart/script.js
@@ -1,22 +1,25 @@
+//-- data -- //
 import data from './data.js';
-// console.log("script.js :: data??\n", data);
-// const test = d3.groups(data, d => d.name);
-// console.log("script.js :: test\n", test);
-
 const dataByCom = d3.groups(data, d => d.name).map(([key, values]) => {
     const v = values[0].value;
     return {key, values: values.map(({date, value}) => ({date, value: value / v}))};
 });
-console.log("script.js :: dataByCom??\n", dataByCom);
+// console.log("script.js :: data??\n", data);
+// const test = d3.groups(data, d => d.name);
+// console.log("script.js :: test\n", test);
+// console.log("script.js :: dataByCom??\n", dataByCom);
 
 const margin = {top: 20, right: 40, bottom: 30, left: 40};
-const height = document.querySelector('.chart').offsetHeight; 
-const width = document.querySelector('.chart').offsetWidth; 
-// console.log("width?", width);    //1170
-// console.log("height?", height);  //600
-// console.log("script.js :: d3?? ", d3)
+//-- 20px offset in both left and right (20x2 = 40) for .svg inside .chart-container
+const width = document.querySelector('.chart-container').offsetWidth - 40; 
+const height = document.querySelector('.chart-container').offsetHeight; 
+// console.log("width?", width); //-- variable  
+// console.log("height?", height); //-- 600 fixed
+
+const bisect = d3.bisector(d => d.date).left;
 
 const k = d3.max(d3.group(data, d => d.name), ([, group]) => d3.max(group, d => d.value) / d3.min(group, d => d.value));
+console.log("script.js :: k?? ", k)
 
 const x = d3.scaleUtc()
     .domain(d3.extent(data, d => d.date))
@@ -27,7 +30,8 @@ const y = d3.scaleLog()
     .domain([1 / k, k])
     .rangeRound([height - margin.bottom, margin.top]);
 
-const z = d3.scaleOrdinal(d3.schemeCategory10).domain(data.map(d => d.name));
+const z = d3.scaleOrdinal(d3.schemeCategory10)
+    .domain(data.map(d => d.name));
 
 const xAxis = g => g
     .attr("transform", `translate(0,${height - margin.bottom})`)
@@ -43,26 +47,33 @@ const yAxis = g => g
     .attr("x2", width - margin.left - margin.right))
     .call(g => g.select(".domain").remove());
 
-const parseDate = d3.utcParse("%Y-%m-%d");
-const formatDate = d3.utcFormat("%B, %Y");
+// const parseDate = d3.utcParse("%Y-%m-%d");
+// const formatDate = d3.utcFormat("%B, %Y");
 
 const line = d3.line()
     .x(d => x(d.date))
-    .y(d => y(d.value));
-
-const bisect = d3.bisector(d => d.date).left;
-
+    .y(d => y(d.value))
+   
 
 
+
+
+//-- svg --//
 const svg = d3.select("svg")
+    // .style("-webkit-tap-highlight-color", "transparent")
     .on("mousemove", moved);
+
+svg.append("g")
+    .call(xAxis);
+svg.append("g")
+    .call(yAxis);
 
 const rule = svg.append("g")
     .append("line")
     .attr("y1", height)
     .attr("y2", 0)
     .attr("stroke", "black");
-  
+
 const series = svg.append("g")
     .style("font", "bold 10px sans-serif")
     .selectAll("g")
@@ -75,59 +86,35 @@ series.append("path")
     .attr("stroke-linejoin", "round")
     .attr("stroke-linecap", "round")
     .attr("stroke", d => z(d.key))
-    .attr("d", d => line(d.values));
+    .attr("d", d => line(d.values)); //ERROR
 
-// series.append("text")
-//     .datum(d => ({key: d.key, value: d.values[d.values.length - 1].value}))
-//     .attr("fill", "none")
-//     .attr("stroke", "white")
-//     .attr("stroke-width", 3)
-//     .attr("x", x.range()[1] + 3)
-//     .attr("y", d => y(d.value))
-//     .attr("dy", "0.35em")
-//     .text(d => d.key)
-//     .clone(true)
-//     .attr("fill", d => z(d.key))
-//     .attr("stroke", null);
+series.append("text")
+    .datum(d => ({key: d.key, value: d.values[d.values.length - 1].value}))
+    .attr("fill", "none")
+    .attr("stroke", "white")
+    .attr("stroke-width", 1)
+    .attr("x", x.range()[1] + 3)
+    .attr("y", d => y(d.value))
+    .attr("dy", "0.35em")
+    .text(d => d.key)
+    .clone(true)
+    .attr("fill", d => z(d.key))
+    .attr("stroke", null);
 
-// series.append("text")
-//     .datum(d => ({key: d.key, value: d.values[d.values.length - 1].value}))
-//     .attr("fill", "none")
-//     .attr("stroke", "white")
-//     .attr("stroke-width", 3)
-//     .attr("x", x.range()[1] + 3)
-//     .attr("y", d => y(d.value))
-//     .attr("dy", "0.35em")
-//     .text(d => d.key)
-//     .clone(true)
-//     .attr("fill", d => z(d.key))
-//     .attr("stroke", null);
 
-const init = () => {
-    console.log("script.js :: init");
-    // svg.append("g")
-    //     .call(xAxis);
-    // svg.append("g")
-    //     .call(yAxis);
-}
-
-function moved (e) {
-    console.log("script.js :: moved");
-    // update(x.invert(d3.pointer(e, this)[0]));
+function moved (event) {
+    // console.log("script.js :: moved");
+    update(x.invert(d3.pointer(event, this)[0]));   //ERROR
     // d3.event.preventDefault();
 }
 
 function update(date) {
-    console.log("script.js :: update");
-    // date = d3.utcDay.round(date);
-    // rule.attr("transform", `translate(${x(date) + 0.5},0)`);
-    // series.attr("transform", ({values}) => {
-    //   const i = bisect(values, date, 0, values.length - 1);
-    //   return `translate(0,${y(1) - y(values[i].value / values[0].value)})`;
-    // });
-    // svg.property("value", date).dispatch("input");
+    // console.log("script.js :: update");
+    date = d3.utcDay.round(date);
+    rule.attr("transform", `translate(${x(date) + 0.5},0)`);    //ERROR
+    series.attr("transform", ({values}) => {
+      const i = bisect(values, date, 0, values.length - 1);
+      return `translate(0,${y(1) - y(values[i].value / values[0].value)})`;
+    });
+    svg.property("value", date).dispatch("input");
 }
-
-
-init();
-
