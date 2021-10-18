7fbda2c80a5eb0f4a3fffa5549985488f2180abd
Whoin Lee <whoinlee@gmail.com>
2021-10-18 19:04:33 -0400

    restructured, slide-container & id  removed

--- p_003_1016-VerticalSlider/script.js
+++ p_003_1016-VerticalSlider/script.js
@@ -1,18 +1,21 @@
 import data from './data.js';
 
 const totalSlides = data.length;
-const slideLeft = document.querySelector('.left-slide > .slide-container');
-const slideRight = document.querySelector('.right-slide > .slide-container');
+const slideLeft = document.querySelector('.left-slide');
+const slideRight = document.querySelector('.right-slide');
+// const slideLeft = document.querySelector('.left-slide > .slide-container');
+// const slideRight = document.querySelector('.right-slide > .slide-container');
 
 //-- build slideLeft
 /*  <div style="background-color: #fd3555">
         <h1>Nature flower</h1>
         <p>all in pink</p>
     </div> */
+slideLeft.innerHTML = '';
 slideLeft.innerHTML = data.map((item, index) => {
     let status = (index === 0)? 'active': 'prev';
     if (index === totalSlides - 1) status = 'next';
-    return `<div class="slide ${status}" data-id="${item.id}" style="background-color: ${item.bgColor}">
+    return `<div class="slide ${status}" style="background-color: ${item.bgColor}">
         <h1>${item.title}</h1>
         <p>${item.sub}</p>
     </div>`}
@@ -20,10 +23,11 @@ slideLeft.innerHTML = data.map((item, index) => {
 
 //-- build slideRight
 /* <div style="background-image: url('https://images.unsplash.com/photo-1508768787810-6adc1f613514?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e27f6661df21ed17ab5355b28af8df4e&auto=format&fit=crop&w=1350&q=80')"></div> */
+slideRight.innerHTML = '';
 slideRight.innerHTML = data.map((item, index) => {
     let status = (index === 0)? 'active': 'next';
     if (index === totalSlides - 1) status = 'prev';
-    return `<div class="slide ${status}" data-id="${item.id}" style="background-image: url(${item.img}">
+    return `<div class="slide ${status}" style="background-image: url(${item.img}">
      </div>`}
 ).join("");
 
