85d9832465b9a34ea013b8b4a8a122766b2052e7
Whoin Lee <whoinlee@gmail.com>
2021-10-18 12:04:56 -0400

    changed to dynamic data loading

---
+++ p_003_1016-VerticalSlider/data.js
@@ -0,0 +1,32 @@
+const data = [{
+    "id": 0,
+    "title": "Nature flower",
+    "sub": "all in pink",
+    "bgColor": "#fd3555",
+    "img": "https://images.unsplash.com/photo-1508768787810-6adc1f613514?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e27f6661df21ed17ab5355b28af8df4e&auto=format&fit=crop&w=1350&q=80",
+    "desc": "carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird "
+  },
+  {
+    "id": 1,
+    "title": "Blue Sky",
+    "sub": "with it's mountains",
+    "bgColor": "#2a86ba",
+    "img": "https://images.unsplash.com/photo-1519981593452-666cf05569a9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=90ed8055f06493290dad8da9584a13f7&auto=format&fit=crop&w=715&q=80",
+  },
+  {
+    "id": 2,
+    "title": "Lonely castle",
+    "sub": "in the wilderness",
+    "bgColor": "#252e33",
+    "img": "https://images.unsplash.com/photo-1486899430790-61dbf6f6d98b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8ecdee5d1b3ed78ff16053b0227874a2&auto=format&fit=crop&w=1002&q=80"
+  },
+  {
+    "id": 3,
+    "title": "Flying eagle",
+    "sub": "in the sunset",
+    "bgColor": "#ffb866",
+    "img": "https://images.unsplash.com/photo-1510942201312-84e7962f6dbb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=da4ca7a78004349f1b63f257e50e4360&auto=format&fit=crop&w=1050&q=80"
+  }
+]
+
+export default data;
--- p_003_1016-VerticalSlider/script.js
+++ p_003_1016-VerticalSlider/script.js
@@ -1,15 +1,33 @@
+import data from './data.js';
+
 const sliderContainer = document.querySelector('.container');
 const slideLeft = document.querySelector('.left-slide');
 const slideRight = document.querySelector('.right-slide');
-const upButton = document.querySelector('.up-button');
-const downButton = document.querySelector('.down-button');
-const totalSlides = slideRight.querySelectorAll('div').length;  //****//
 
+
+//-- build slideLeft
+/*  <div style="background-color: #fd3555">
+        <h1>Nature flower</h1>
+        <p>all in pink</p>
+    </div> */
+slideLeft.innerHTML = data.map(item => 
+    `<div style="background-color: ${item.bgColor}">
+        <h1>${item.title}</h1>
+        <p>${item.sub}</p>
+     </div>`
+).join("");
+
+//-- build slideRight
+/* <div style="background-image: url('https://images.unsplash.com/photo-1508768787810-6adc1f613514?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e27f6661df21ed17ab5355b28af8df4e&auto=format&fit=crop&w=1350&q=80')"></div> */
+slideRight.innerHTML = data.map(item => 
+    `<div style="background-image: url(${item.img}">
+     </div>`
+).join("");
+
+
+const totalSlides = data.length;
 let activeSlideIndex = 0;
 slideLeft.style.top = `-${(totalSlides - 1)*100}vh`;
-
-upButton.addEventListener('click', (e)=>{e.preventDefault();changeSlide('up')});
-downButton.addEventListener('click', (e)=>{e.preventDefault();changeSlide('down')});
 
 const changeSlide = (direction) => {
     const sliderHeight = sliderContainer.clientHeight;
@@ -23,4 +41,9 @@ const changeSlide = (direction) => {
         slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
     }
     // console.log("activeSlideIndex? " + activeSlideIndex)
-}
+};
+
+const upButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
+upButton.addEventListener('click', (e)=>{e.preventDefault();changeSlide('up')});
+downButton.addEventListener('click', (e)=>{e.preventDefault();changeSlide('down')});
