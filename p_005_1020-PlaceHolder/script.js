b3bdcbb33e62f3f5861d9d671c6d2b1783baf73d
Whoin Lee <whoinlee@gmail.com>
2021-10-21 09:52:26 -0400

    revisit

--- p_005_1020-PlaceHolder/script.js
+++ p_005_1020-PlaceHolder/script.js
@@ -11,18 +11,17 @@ const animated_bgs = document.querySelectorAll('.animated-bg');
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text');
 
 function getData() {
-    header.innerHTML = '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />';
+    header.innerHTML = '<img class="show" src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />';
     title.innerHTML = 'Lorem ipsum dolor sit amet';
 
     excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis';
-    profileImg.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
+    profileImg.innerHTML = '<img class="show" src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
 
     userName.innerHTML = 'John Doe';
     date.innerHTML = 'Oct 08, 2020';
 
     animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'));
     animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'));
-  
 };
 
 setTimeout(getData, 2500);  //-- background animation stops in 2.5 sec, when data is loaded
