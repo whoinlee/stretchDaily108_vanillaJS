e9fac288add8427de0400e2d3e5e9900301da423
Whoin Lee <whoinlee@gmail.com>
2021-10-22 06:21:31 -0400

    container div added to fix long list hiding bug

--- p_015_0826-ToDoList/script.js
+++ p_015_0826-ToDoList/script.js
@@ -1,10 +1,9 @@
-const form = document.getElementById('form')
-const input = document.getElementById('input')
-const todosUL = document.getElementById('todos')
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todosUL = document.getElementById('todos');
 
 const todos = JSON.parse(localStorage.getItem('todos'));    //******/
-console.log("localStorage.getItem('todos')", localStorage.getItem('todos'))
-
+// console.log("localStorage.getItem('todos')", localStorage.getItem('todos'))
 if (todos) {
     todos.forEach(todo => addTodo(todo));
 }
@@ -12,46 +11,49 @@ if (todos) {
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     addTodo();
+    input.value="";
 })
 
 function addTodo(todo) {
-    let todoText = input.value;
+    let todoText = input.value.trim();
+    if (todo) { todoText = todo.text };
 
-    if (todo) { todoText = todo.text }
     if (todoText) {
-        const todoLI = document.createElement('li');//
+        const todoLI = document.createElement('li');
         if (todo && todo.completed) {
-            todoLI.classList.add('completed')
+            //-- from localStorage
+            todoLI.classList.add('completed');
         };
-        todoLI.innerText = todoText
+        todoLI.innerText = todoText;
+
+        //-- left click
         todoLI.addEventListener('click', () => {
-            todoLI.classList.toggle('completed')
-            updateLS()
+            todoLI.classList.toggle('completed');
+            updateLocalStorage();
         });
         //-- right click
         todoLI.addEventListener('contextmenu', (e) => {
-            e.preventDefault()
-            todoLI.remove()
-            updateLS()
+            e.preventDefault();
+            todoLI.remove();
+            updateLocalStorage();
         }) 
         todosUL.appendChild(todoLI);
 
         input.value = '';   //-- reset
-        updateLS();
+        updateLocalStorage();
     }
-}
+};
 
-function updateLS() {
-    todosEl = document.querySelectorAll('li')
+function updateLocalStorage() {
+    todosEl = document.querySelectorAll('li');
 
-    const todos = []
-
+    const todos = [];
     todosEl.forEach(todoLI => {
         todos.push({
             text: todoLI.innerText,
             completed: todoLI.classList.contains('completed')
         })
-    })
+    });
 
     localStorage.setItem('todos', JSON.stringify(todos));   //-- save to the local storage
-}
+};
