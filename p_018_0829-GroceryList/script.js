62ccec3d0e66267083505073c6586cb21fabec4e
Whoin Lee <whoinlee@gmail.com>
2021-10-23 10:32:28 -0400

    p18 revisited

--- p_018_0829-GroceryList/script.js
+++ p_018_0829-GroceryList/script.js
@@ -6,7 +6,8 @@ const submitBtn = document.querySelector(".submit-btn");
 const container = document.querySelector(".grocery-container");
 const list = document.querySelector(".grocery-list");
 const clearBtn = document.querySelector(".clear-btn");
-//
+
+
 let editElement;
 let editFlag = false;
 let editID = "";
