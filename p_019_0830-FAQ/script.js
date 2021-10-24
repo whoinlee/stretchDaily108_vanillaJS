b6f7965ffb0bd12534ef79ae08762d318fc5f327
Whoin Lee <whoinlee@gmail.com>
2021-10-24 10:21:41 -0400

    setupFAQ updated

--- p_019_0830-FAQ/script.js
+++ p_019_0830-FAQ/script.js
@@ -1,48 +1,34 @@
 const container = document.querySelector(".faq-container");
-let selectedFaq;
 
 const setupFAQs = async() => {
-    // console.log("setupFAQs");
-
     //-- get data
     const res = await fetch('./data.json');  
     const faqData = await res.json();  //-- an array of objects
-    if (faqData != null && faqData.length > 0) {
-        faqData.forEach(item => {
-            const faqElt = document.createElement("div");
-            let attr = document.createAttribute("data-id");
-            attr.value = item.id;
-            faqElt.setAttributeNode(attr);
-            faqElt.classList.add("faq");
-            faqElt.innerHTML = `
-                <!-- .faq-question -->
+    if (faqData && faqData.length > 0) {
+        container.innerHTML = faqData.map(item => `
+            <div data-id=${item.id} class="faq">
                 <h3 class="faq-question">
                     ${item.question}
                 </h3>
-                <!-- .faq-answer -->
                 <p class="faq-answer">
                     ${item.answer}
                 </p>
-                <!-- .faq-toggle-btn -->
                 <button class="faq-toggle-btn">
                     <i class="fas fa-chevron-down"></i>
                     <i class="fas fa-times"></i>
                 </button>
-            `;
-            const toggleBtn = faqElt.querySelector(".faq-toggle-btn");
-            toggleBtn.addEventListener("click", toggleFAQ);
-            container.appendChild(faqElt);
-        });
-      }
+            </div>
+        `).join('');
+        const toggleBtns = container.querySelectorAll(".faq-toggle-btn");
+        toggleBtns.forEach(toggleBtn => toggleBtn.addEventListener("click", toggleFAQ));
+    }
 };
 
+let selectedFaq;
 const toggleFAQ = (e) => {
-    // console.log("toggleFAQ");
     e.preventDefault();
     
     const element = e.currentTarget.parentElement;  //-- .faq div
-    //element.classList.toggle('active');
-
     if (element.classList.contains("active")) {
         element.classList.remove("active");
         selectedFaq = null;
@@ -52,4 +38,5 @@ const toggleFAQ = (e) => {
         selectedFaq = element;
     }
 }
+
 setupFAQs();
