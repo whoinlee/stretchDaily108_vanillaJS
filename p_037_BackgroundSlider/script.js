84cc70082a0714cd30802f14eee03d13d7488699
Whoin Lee <whoinlee@gmail.com>
2021-10-27 21:25:32 -0400

    script updated

--- p_037_BackgroundSlider/script.js
+++ p_037_BackgroundSlider/script.js
@@ -1,33 +1,37 @@
+// import slides from "./slides.js";
+
 const body = document.body;
-const slides = document.querySelectorAll('.slide');
-const leftBtn = document.getElementById('leftBtn');
-const rightBtn = document.getElementById('rightBtn');
+const slideArr = document.querySelectorAll(".slide");
+const leftBtn = document.getElementById("leftBtn");
+const rightBtn = document.getElementById("rightBtn");
 
 let activeSlideIndex = 0;
 setBgToBody();
 
-rightBtn.addEventListener('click', () => {
-    activeSlideIndex++;
-    if (activeSlideIndex > slides.length - 1) {
-        activeSlideIndex = 0
-    }
-    setBgToBody();
-    setActiveSlide();
-});
-leftBtn.addEventListener('click', () => {
-    activeSlideIndex--;
-    if (activeSlideIndex < 0) {
-        activeSlideIndex = slides.length - 1
-    }
-    setBgToBody();
-    setActiveSlide();
+function setBgToBody() {
+  //-- copy src
+  body.style.backgroundImage = slideArr[activeSlideIndex].style.backgroundImage;
+}
+
+function setActiveSlide() {
+  slideArr.forEach((slide) => slide.classList.remove("active"));
+  slideArr[activeSlideIndex].classList.add("active");
+}
+
+rightBtn.addEventListener("click", () => {
+  activeSlideIndex++;
+  if (activeSlideIndex > slideArr.length - 1) {
+    activeSlideIndex = 0;
+  }
+  setBgToBody();
+  setActiveSlide();
 });
 
-function setBgToBody() {
-    //-- copy src
-    body.style.backgroundImage = slides[activeSlideIndex].style.backgroundImage;
-};
-function setActiveSlide() {
-  slides.forEach((slide) => slide.classList.remove('active'));
-  slides[activeSlideIndex].classList.add('active');
-};
+leftBtn.addEventListener("click", () => {
+  activeSlideIndex--;
+  if (activeSlideIndex < 0) {
+    activeSlideIndex = slideArr.length - 1;
+  }
+  setBgToBody();
+  setActiveSlide();
+});
---
+++ p_037_BackgroundSlider/slides.js
@@ -0,0 +1,24 @@
+const slides = [
+  {
+    id: 1,
+    img: "'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1511593358241-7eea1f3c84e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1495467033336-2effd8753d51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
+  },
+  {
+    id: 4,
+    img: "https://images.unsplash.com/photo-1522735338363-cc7313be0ae0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2689&q=80",
+  },
+  {
+    id: 5,
+    img: "https://images.unsplash.com/photo-1559087867-ce4c91325525?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
+  },
+];
+
+export default slides;
