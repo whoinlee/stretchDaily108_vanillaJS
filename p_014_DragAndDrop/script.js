40e2a9f67b0f876aca149a8b5d1b25609f279033
Whoin Lee <whoinlee@gmail.com>
2021-10-29 13:51:24 -0400

    extra pixel issue on holding fixed

--- p_014_DragAndDrop/script.js
+++ p_014_DragAndDrop/script.js
@@ -1,39 +1,34 @@
-const empties = document.querySelectorAll('.empty');
-const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll(".empty");
+const fill = document.querySelector(".fill");
 
-fill.addEventListener('dragstart', dragStart)
-fill.addEventListener('dragend', dragEnd)
-
-for(const empty of empties) {
-    empty.addEventListener('dragover', dragOver)
-    empty.addEventListener('dragenter', dragEnter)
-    empty.addEventListener('dragleave', dragLeave)
-    empty.addEventListener('drop', dragDrop)
-}
+empties.forEach((empty) => {
+  empty.addEventListener("dragover", (e) => e.preventDefault());
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+});
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
 
 function dragStart() {
-    this.className += ' hold'; //*****/
-    setTimeout(() => this.className = '', 0);  //?????
+  this.className += " hold"; //*****/
+  setTimeout(() => (this.className = ""), 0); //?????
 }
 
 function dragEnd() {
-    this.className = 'fill';
-}
-
-function dragOver(e) {
-    e.preventDefault();
+  this.className = "fill";
 }
 
 function dragEnter(e) {
-    e.preventDefault();
-    this.className += ' hovered';
+  e.preventDefault();
+  this.className += " hovered";
 }
 
 function dragLeave() {
-    this.className = 'empty';
+  this.className = "empty";
 }
 
 function dragDrop() {
-    this.className = 'empty';
-    this.append(fill);  //******/
+  this.className = "empty";
+  this.append(fill); //******/
 }
