e5345c3505bc939037dbc9127eff98b2ca450bbd
Whoin Lee <whoinlee@gmail.com>
2021-11-08 16:51:46 -0500

    style updated to the row direction

--- p_041_Notification/script.js
+++ p_041_Notification/script.js
@@ -1,30 +1,25 @@
-const button = document.getElementById('button');
-const toasts = document.getElementById('toasts');
-const messages = [
-    'Message One',
-    'Message Two',
-    'Message Three',
-    'Message Four',
-];
-const types = ['info', 'success', 'error'];
+const button = document.getElementById("button");
+const toasts = document.getElementById("toasts");
+const messages = ["Msg 1", "Msg 2", "Msg 3", "Msg 4", "Msg 5"];
+const types = ["info", "success", "error"];
 
-button.addEventListener('click', () => createNotification());
+button.addEventListener("click", () => createNotification());
 function createNotification(message = null, type = null) {
-    const notif = document.createElement('div');
-    notif.classList.add('toast');
-    notif.classList.add(type ? type : getRandomType());
-    notif.innerText = message ? message : getRandomMessage();
-    toasts.appendChild(notif)
+  const notif = document.createElement("div");
+  notif.classList.add("toast");
+  notif.classList.add(type ? type : getRandomType());
+  notif.innerText = message ? message : getRandomMessage();
+  toasts.appendChild(notif);
 
-    setTimeout(() => {
-        notif.remove()
-    }, 3000)
-};
+  setTimeout(() => {
+    notif.remove();
+  }, 3000);
+}
 
 function getRandomMessage() {
-    return messages[Math.floor(Math.random() * messages.length)]
-};
+  return messages[Math.floor(Math.random() * messages.length)];
+}
 
 function getRandomType() {
-    return types[Math.floor(Math.random() * types.length)]
-};
+  return types[Math.floor(Math.random() * types.length)];
+}
