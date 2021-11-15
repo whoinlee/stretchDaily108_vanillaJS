88c5ba5f85be5ed1046f6a3ea56bc8f2ee769581
Whoin Lee <whoinlee@gmail.com>
2021-11-15 14:02:23 -0500

    rough complete

--- p_064_1115_InteractiveDonutChart/script.js
+++ p_064_1115_InteractiveDonutChart/script.js
@@ -1,41 +1,43 @@
-//-- originally from
-//-- https://bl.ocks.org/jonsadka/fa05f8d53d4e8b5f262e
+//-- updated from original source: https://bl.ocks.org/jonsadka/fa05f8d53d4e8b5f262e
 
-var width = 960,
-  height = 500 - 29; // adjust for height of input bar div
-var color = d3.scale.category20();
-// draw and append the container
-var svg = d3.select("svg").attr("width", width).attr("height", height);
-// set the thickness of the inner and outer radii
-var min = Math.min(width, height);
-var oRadius = (min / 2) * 0.9;
-var iRadius = (min / 2) * 0.85;
-// construct default pie laoyut
-var pie = d3.layout
+const width = 960;
+const height = 500;
+const min = Math.min(width, height);
+const oRadius = (min / 2) * 0.9; //-- outer
+const iRadius = (min / 2) * 0.85; //-- inner
+const color = d3.scale.category20();
+
+//-- set svg width and height
+const svg = d3.select("svg").attr("width", width).attr("height", height);
+
+//-- construct default pie laoyut
+const pie = d3.layout
   .pie()
-  .value(function (d) {
-    return d;
-  })
+  .value((d) => d)
   .sort(null);
-// construct arc generator
-var arc = d3.svg.arc().outerRadius(oRadius).innerRadius(iRadius);
-// creates the pie chart container
-var g = svg.append("g");
-var g = svg.append("g").attr("transform", function () {
-  if (window.innerWidth >= 960) var shiftWidth = width / 2;
-  if (window.innerWidth < 960) var shiftWidth = width / 3;
-  return "translate(" + shiftWidth + "," + height / 2 + ")";
-});
-// generate random data
-var data = makeData(+document.getElementById("datacount").value);
-// enter data and draw pie chart
-var path = g
+
+//-- construct arc generator
+const arc = d3.svg.arc().outerRadius(oRadius).innerRadius(iRadius);
+
+//-- construct group to hold pie chart and set/transform the location to the center of the screen
+const g = svg
+  .append("g")
+  .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+//-- generate [size] numbers of random numbers between 0 and 100
+const makeData = (size) => d3.range(size).map((item) => Math.random() * 100);
+//-- generate random data based on input value (***var***, reset in the render function)
+let data = makeData(+document.getElementById("datacount").value);
+// console.log("data: \n" + data);
+
+//-- enter data and draw pie chart
+const path = g
   .datum(data)
   .selectAll("path")
   .data(pie)
   .enter()
   .append("path")
-  .attr("class", "piechart")
+  //   .attr("class", "piechart")
   .attr("fill", function (d, i) {
     return color(i);
   })
@@ -43,6 +45,18 @@ var path = g
   .each(function (d) {
     this._current = d;
   });
+
+//-- Store the displayed angles in _current.
+//-- Then, interpolate from _current to the new angles.
+//-- During the transition, _current is updated in-place by d3.interpolate.
+function arcTween(a) {
+  var i = d3.interpolate(this._current, a);
+  this._current = i(0);
+  return function (t) {
+    return arc(i(t));
+  };
+}
+
 function render() {
   // generate new random data
   data = makeData(+document.getElementById("datacount").value);
@@ -70,20 +84,6 @@ function render() {
   // remove data not being used
   g.datum(data).selectAll("path").data(pie).exit().remove();
 }
+
 render();
 setInterval(render, 2000);
-function makeData(size) {
-  return d3.range(size).map(function (item) {
-    return Math.random() * 100;
-  });
-}
-// Store the displayed angles in _current.
-// Then, interpolate from _current to the new angles.
-// During the transition, _current is updated in-place by d3.interpolate.
-function arcTween(a) {
-  var i = d3.interpolate(this._current, a);
-  this._current = i(0);
-  return function (t) {
-    return arc(i(t));
-  };
-}
