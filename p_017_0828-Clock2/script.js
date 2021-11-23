23e4fe09a9a725719318ac16fe9b1875e716fda9
Whoin Lee <whoinlee@gmail.com>
2021-11-23 07:54:29 -0500

    p17 revisited

--- p_017_0828-Clock2/script.js
+++ p_017_0828-Clock2/script.js
@@ -1,21 +1,21 @@
-const hourHand = document.getElementById('hour-hand');
-const minuteHand = document.getElementById('minute-hand');
-const secondHand = document.getElementById('second-hand');
+const hourHand = document.getElementById("hour-hand");
+const minuteHand = document.getElementById("minute-hand");
+const secondHand = document.getElementById("second-hand");
 
 function setTime() {
-    const time = new Date();
-    const secondDeg = time.getSeconds()/60; 
-    const minuteDeg = (secondDeg + time.getMinutes())/60;
-    const hourDeg = (minuteDeg + time.getHours())/12;
+  const time = new Date();
+  const secondDeg = time.getSeconds() / 60;
+  const minuteDeg = (secondDeg + time.getMinutes()) / 60;
+  const hourDeg = (minuteDeg + time.getHours()) / 12;
 
-    handsDegs(hourHand, hourDeg);
-    handsDegs(minuteHand, minuteDeg);
-    handsDegs(secondHand, secondDeg);
+  handsDegs(hourHand, hourDeg);
+  handsDegs(minuteHand, minuteDeg);
+  handsDegs(secondHand, secondDeg);
 }
 
 function handsDegs(elt, rotationRatio) {
-    elt.style.setProperty('--rotation', rotationRatio * 360);
+  elt.style.setProperty("--rotation", rotationRatio * 360);
 }
 
 setTime();
-// setInterval(setTime, 1000);
+setInterval(setTime, 1000);
