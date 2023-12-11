9c3d375c2aa84e91b2ae69f5249de7446412c58e
WhoIN Lee <whoinlee@gmail.com>
2023-12-10 20:59:42 -0500

    p066 complete

---
+++ p_066_imageGallery/script.js
@@ -0,0 +1,37 @@
+const current = document.querySelector('#current');
+const imgs = document.querySelector('.imgs');
+const img = document.querySelectorAll('.imgs img');
+const opacity = 0.6;
+let selectedThumb  = img[0];
+
+// Set first img opacity
+selectedThumb.style.opacity = opacity;
+
+imgs.addEventListener('click', imgClick);
+
+function imgClick(e) {
+    e.preventDefault();
+    // Reset the opacity
+    selectedThumb.style.opacity = 1;
+
+    // Change current image to src of clicked image
+    current.src = e.target.src;
+    current.classList.value = "";
+
+    // Add fade in class
+    current.classList.add('fade-in');
+
+  // Remove fade-in class after .5 seconds
+//   setTimeout(() => current.classList.remove('fade-in'), 500);
+
+  // Change the opacity to opacity var
+  selectedThumb = e.target;
+  selectedThumb.style.opacity = opacity;
+}
+
+for (let i=0; i<4; i++) {
+    const newImg = document.createElement("img");
+    newImg.setAttribute("src", "https://preview.ibb.co/gxVppG/img1.jpg")
+    imgs.append(newImg);
+}
+
